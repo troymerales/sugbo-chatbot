8fd9620e50130679d5812b4194d4828cadcc5b6a
Benedicto Merales Jr <troymerales@gmail.com>
2026-09-14 16:06:23 +0800

    run resiliency

--- deploy/asr-space/app.py
+++ deploy/asr-space/app.py
@@ -56,4 +56,10 @@ demo = gr.Interface(
 )
 
 if __name__ == "__main__":
-    demo.launch(share=False)
+    demo.launch(
+        server_name="0.0.0.0",
+        server_port=7860,
+        share=False,
+        show_error=True,
+        show_api=True,
+    )
