7a7bcf815a5a45edfc65d1ba00f96ad9016e5f14
Benedicto Merales Jr <troymerales@gmail.com>
2026-09-15 08:50:53 +0800

    disable SSR

--- deploy/asr-space/app.py
+++ deploy/asr-space/app.py
@@ -62,6 +62,7 @@ if __name__ == "__main__":
             server_name="0.0.0.0",
             server_port=7860,
             share=False,
+            ssr_mode=None,
         )
     except Exception as e:
         print(f"ERROR: {e}", flush=True)
